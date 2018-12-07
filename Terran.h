40a287b2533183696ca7b8044c9630d883624228
ul40ajet <ul40ajet@fauerlnue.onmicrosoft.com>
2018-12-07 18:48:22 +0100

    Terran.h basics added

---
+++ Terran.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <map>
+
+#include "Race.h"
+
+using namespace std;
+
+class Terran : public Race{
+
+public:
+    Terran() {}
+
+    ~Terran() {}
+
+private:
+    
+
+
+    void run (){
+        for(int timestep = 1; timestep < 1000; ++timestep){
+
+
+        }
+        return 1;
+    }
+
+
+
+}
