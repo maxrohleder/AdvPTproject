4beba6873ac0079265bc8d8715891b9f72a07ce7
ef95usus <philip.ps.stoll@fau.de>
2018-12-26 09:56:56 +0100

    trying some changes to validator

--- Validator/validatorTest.cpp
+++ Validator/validatorTest.cpp
@@ -2,10 +2,8 @@
 #include "ValidatorZerg.h"
 
 int main(){
-    cout << "here wtf" << endl;
     ZergChecker zc;
-    cout << "init done" << endl;
     bool valid = zc.runDebug({"drone", "drone"});
     cout << valid << endl;
-    return 0;
+    return 0; 
 }
