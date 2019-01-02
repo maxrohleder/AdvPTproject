314deea63021191562e267d6e0ddcbea836ad4ca
ef95usus <philip.ps.stoll@fau.de>
2019-01-02 22:33:33 +0100

    new flag usage of validator test

--- Validator/validatorTest.cpp
+++ Validator/validatorTest.cpp
@@ -3,22 +3,39 @@
 #include <string>
 
 int main(int argc, char* argv[]){
-    if(argc < 2){
+    if(argc != 3){
         cout << "Usage : " << argv[0] << " <flag> <inputfile>" << endl;
         exit(-1);
     }
-    ZergChecker zc;
+    bool debug = false;
     bool valid = false;
-    if(argc > 2){
-        if(string(argv[1]) != "-d"){
-            cout << "invalid flag (try -d)" << endl;
-            exit(-1);
-        }
-        valid = zc.runDebug(argv[2]);
-    }else{
-        valid = zc.run(argv[1]);
+    if(string(argv[1]).find('d') != string::npos){
+        debug = true;
     }
-    if (valid) cout << "\nvalid" << endl;
-    else cout << "\ninvalid" << endl;
-    return 0; 
+    if(string(argv[1]).find('t') != string::npos){
+        //create TerranChecker tc
+        if (debug) //valid = tc.runDebug(argv[2]);
+        //else valid = tc.run(argv[2]);
+        if (valid) cout << "\nvalid" << endl;
+        else cout << "\ninvalid" << endl;
+        return 0;
+    }
+    if(string(argv[1]).find('z') != string::npos){
+        ZergChecker zc;
+        if(debug) valid = zc.runDebug(argv[2]);
+        else valid = zc.run(argv[2]);
+        if (valid) cout << "\nvalid" << endl;
+        else cout << "\ninvalid" << endl;
+        return 0; 
+    }
+    if(string(argv[1]).find('p') != string::npos){
+        //create ProtossChecker pc
+        if(debug) //valid = pc.runDebug(argv[2]);
+        //else valid = pc.run(argv[2]);
+        if (valid) cout << "\nvalid" << endl;
+        else cout << "\ninvalid" << endl;
+        return 0; 
+    }
+    cout << "invalid flag (try -d for debug and -t / -z / -p for Terran / Zerg / Protoss)" << endl;
+    return -1;
 }
