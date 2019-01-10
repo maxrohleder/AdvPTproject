44ca201e054a4812df8d547f9056f415ce6e7350
ul40ajet <ul40ajet@fauerlnue.onmicrosoft.com>
2019-01-10 14:00:26 +0100

    richtige übergabe reihenfolge

--- Terran/mainTerran.cpp
+++ Terran/mainTerran.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 #include "../Terran.h"
 
 using namespace std;
 
-int main(){
-    string a;
-    cin >> a;
-    Terran ter(a);
-    return ter.run();
+int main(int argc, char* argv[]){
+    if(argc < 2){
+        cout << "Usage : " << argv[0] << " <inputfile>" << "" << endl;
+        exit(-1);
+    }
+    Terran ter(argv[1]);
+    ter.run();
+    return 0;
 }
