1b087aab446a98469ad63505cc40392ba4ed9649
ef95usus <philip.ps.stoll@fau.de>
2019-01-19 18:22:02 +0100

    now exiting at problem

--- Validator/list_builder_test.cpp
+++ Validator/list_builder_test.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char* argv[]){
     if(argc < 3){
         cout << "Usage: " << string(argv[0]) << "<path to techtree> <unit to build>" << endl;
+        exit(1);
     }
     list_builder lb(string(argv[1]), 'z');
     lb.runDebug(string(argv[2]));
