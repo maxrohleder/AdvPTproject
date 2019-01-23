91fa41e4495ef6748397836c61e099d3996c2c8c
ef95usus <philip.ps.stoll@fau.de>
2019-01-23 20:04:10 +0100

    new bash scripts for optimization

--- bash_scripts/build_win_old.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#TODO change to unix
-if [ ! -d "build" ]; then
-    mkdir build
-fi
-cd build
-cmake ..
-make
-cd ..
---
+++ bash_scripts/collect_opt.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+cd ..
+if [ ! -d "collection" ]; then
+    mkdir collection
+else
+    rm -r collection
+    mkdir collection
+fi
+
+#dirs for races
+mkdir collection/Protoss
+mkdir collection/Validator
+mkdir collection/Validator/validator_by_file
+mkdir collection/Terran
+mkdir collection/Zerg
+
+#copy over all scripts
+cp CMakeLists.txt collection/
+cp Race.h collection/
+
+#Protoss
+cp Protoss.h collection/
+cp Protoss/CMakeLists.txt collection/Protoss/
+cp Protoss/mainProtoss.cpp collection/Protoss/
+cp Protoss/Protoss_status.h collection/Protoss/
+cp Protoss/parser.h collection/Protoss/
+cp Protoss/techtree_protoss.csv collection/Protoss/
+
+#Terran
+cp Terran.h collection/
+cp Terran_header.h collection/
+cp Terran/CMakeLists.txt collection/Terran/
+cp Terran/mainTerran.cpp collection/Terran/
+
+#Zerg
+cp Zerg.h collection/
+cp Zerg_header.h collection/
+cp Zerg/CMakeLists.txt collection/Zerg/
+cp Zerg/mainZerg.cpp collection/Zerg/
+
+#Optimization
+cp Optimization/CMakeLists.txt collection/Optimization
+cp Optimization/global_enums.h collection/Optimization
+cp Optimization/list_builder.h collection/Optimization
+cp Optimization/mutations.h collection/Optimization
+cp Optimization/natural_selection.h collection/Optimization
+cp Optimization/Opt.h collection/Optimization
+cp Optimization/parser.h collection/Optimization
+cp Optimization/techtree* collection/Optimization
+
+
+#copy bash scrpits
+cp bash_scripts/build.sh collection/
+cp bash_scripts/forwardSim.sh collection/
+cp bash_scripts/optimization.sh collection/
+ 
+cd bash_scripts
