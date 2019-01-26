b7477854876e2be053b1e8fb016b8bf650787c53
ul40ajet <ul40ajet@fauerlnue.onmicrosoft.com>
2019-01-26 21:41:27 +0100

    correction in terran parser, validatortest added

--- Optimization/natural_selection.h
+++ Optimization/natural_selection.h
@@ -1,7 +1,7 @@
-#pragma once
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <list>
 
 #include "global_enums.h"
 #include "../Race.h"
@@ -31,16 +31,22 @@ class natural_selector{
     Race evaluator;
     public:
     natural_selector(RaceType r) : rt(r){}
-    natural_selector(const &natural_selector n){}
+    //natural_selector(const &natural_selector n){}
     ~natural_selector(){}
 
-    void sortBuildLists(list<pair<list<string>, int>> &buildlists){
+    /* void sortBuildLists(list<pair<list<string>, int>> &buildlists){
 
         buildlists->sort(comp);
-    }
+    } */
 
     void cutNBest(list<pair<list<string>, int>> &buildlists, int n){
-        runBuilists(buildlists);
-        buildlists->resize(n);
+        //runBuilists(buildlists);
+        //buildlists->resize(n);
+
+        list<pair<list<string>, int>>::iterator position_n, position_end;
+        position_n = position_end = buildlists.begin();
+        advance(position_n, n);
+        advance(position_end, buildlists.end());
+        buildlists.erase(position_n, position_end);
     }
-}
+};
