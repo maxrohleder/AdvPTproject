3a77b83c5a9ede830522b2486e3f794ad08811f7
ef95usus <philip.ps.stoll@fau.de>
2019-03-18 20:40:26 +0100

    chenging parameters for zerg

--- Optimization/list_builder.h
+++ Optimization/list_builder.h
@@ -84,12 +84,11 @@ class list_builder{
         multiple.push_back("overlord");
         multiple.push_back("overlord");
         multiple.push_back("overlord");
-        
         //multiple.push_back("queen");
         once.push_back("extractor");
         once.push_back("extractor");
         prob_mult = 15;
-        prob_once = prob_mult + 10;
+        prob_once = prob_mult + 5;
     }
 
 
