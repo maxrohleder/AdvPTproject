e7046f7fce098284ff258fc852f77152991db55b
MR <Maximilian.Rohleder@methodpark.de>
2019-03-24 16:51:51 +0100

    fixing problem in crossbreed

--- Optimization/mutations.h
+++ Optimization/mutations.h
@@ -48,14 +48,15 @@ class Mutator{
     list<string> cross_breed(list<string> list1, list<string> list2){
         // taking one from each list alternatingly
         list<string>::iterator iter1 = list1.begin();
+        ++iter1;
         list<string>::iterator iter2 = list2.begin();
 
         list<string> res = {};
-        while(iter1 != list1.end() and iter2 != list2.end()){
+        while(iter1 != list1.end() && iter2 != list2.end()){
             res.push_back(*iter1);
-            advance(iter1, 2);
+            advance(iter1, min(2, (int) (distance(iter1, list1.end()) - 1));
             res.push_back(*iter2);
-            advance(iter2, 2);
+            advance(iter2, min(2, (int) distance(iter2, list2.end())));
         }
         return res;
     }
@@ -202,9 +203,7 @@ class Mutator{
             auto list2 = buildlists.begin();
             advance(list2, l2);
             if(chance == 0){
-                //res = cross_breed(list1->first, list2->first);
                 res = singleSwap(list1->first, list2->first, target);
-                runAndInsertList(buildlists, res);
             }
             else if(chance == 1){
                 res = mutate(list1->first);
